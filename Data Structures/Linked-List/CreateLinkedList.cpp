fcf453f6f0e34b35b6c1250bb031bef62c2ecb43
ayushtamra <tamraayush@gmail.com>
2022-08-15 12:42:59 +0530

    Updated create linked list
    Signed-off-by: ayushtamra <tamraayush@gmail.com>

--- Data Structures/Linked-List/CreateLinkedList.cpp
+++ Data Structures/Linked-List/CreateLinkedList.cpp
@@ -1,69 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define fast ios::sync_with_stdio(0); cin.tie(0);
+// Create and print a linked list
 
-class ListNode{
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
 public:
-    int data;
+    int val;
     ListNode* next;
 };
 
-ListNode* inputList()
+void printList(ListNode* head)
 {
+    ListNode* tmp = head;
+    for(int i=0;tmp != NULL;i++)
+    {
+        cout<<tmp->val<<" ";
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    ListNode* head = new ListNode();
+    ListNode* prev = head;
     int n;
     cin>>n;
-    ListNode* HEAD = new ListNode();
-    ListNode* ptr;
-    
     for(int i=0;i<n;i++)
     {
-        if(i == 0)
-        {
-            ptr = HEAD;
-        }
-        cin>>ptr->data;
-
-        if(i != n-1) {
-            ListNode* tmp = new ListNode();
-            ptr->next = tmp;
-            ptr = ptr->next;
-        }
+        ListNode* nd = new ListNode;
+        prev->next = nd;
+        cin>>nd->val;
+        prev = nd;
     }
 
-    return HEAD;
-}
-
-void outputList(ListNode* HEAD)
-{
-    ListNode* ptr = HEAD;
-    while(ptr != NULL)
-    {
-        cout<<ptr->data<<" ";
-        ptr = ptr->next;
-    }
-}
-
-void solve()
-{
-    ListNode* HEAD = inputList();
-    outputList(HEAD);
-}
-
-signed main()
-{
-    fast;
-
-#ifndef ONLINE_JUDGE
-    freopen("/home/ayush/Desktop/CP-DSA/input.txt", "r", stdin);
-    freopen("/home/ayush/Desktop/CP-DSA/output.txt", "w", stdout);
-#endif
-
-    int t = 1;
-    cin >> t;
-    for (int tt = 1; tt <= t; tt++)
-    {
-        solve();
-    }
-    return 0;
+    printList(head->next);
 }
